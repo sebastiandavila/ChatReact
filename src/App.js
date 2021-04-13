e8cbdde2e66c5c9986e7ee92334054fcb048642c
@sebas.davila.garcia <sebas.davila.garcia@gmail.com>
2021-04-13 08:22:14 -0500

    chat funcionando

--- src/App.js
+++ src/App.js
@@ -22,10 +22,81 @@ const firestore = firebase.firestore();
 
 function App() {
   const [user] = useAuthState(auth);
-  return <div className="App">{user ? <ChatRoom /> : <SignIn />}</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Sofka Chat</h1>
+        <SignOut />
+      </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
 }
 
-function ChatRoom() {}
+function ChatRoom() {
+  const messageRef = firestore.collection("messages");
+  const query = messageRef.orderBy("createdAt").limitToLast(30);
+  const [messages] = useCollectionData(query, { idField: "id" });
+  const dummy = useRef();
+
+  const [formValue, setFormValue] = useState("");
+
+  useEffect(() => {
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL, displayName } = auth.currentUser;
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      displayName,
+      photoURL,
+    });
+
+    setFormValue("");
+  };
+
+  return (
+    <main>
+      <div>
+        {messages &&
+          messages.map((msn) => <ChatMessage key={msn.id} message={msn} />)}
+      </div>
+      <div>
+        <form onSubmit={sendMessage}>
+          <input
+            value={formValue}
+            onChange={(e) => {
+              setFormValue(e.target.value);
+            }}
+            placeholder="Escribe aquí"
+          />
+          <button type="submit" disabled={!formValue}>
+            Send
+          </button>
+        </form>
+      </div>
+      <span ref={dummy}></span>
+    </main>
+  );
+}
+function ChatMessage({ message }) {
+  const { text, uid, photoURL, displayName } = message;
+
+  const messageOrderClass = uid === auth.currentUser.uid ? "send" : "received";
+
+  return (
+    <div children={"message " + messageOrderClass}>
+      <img src={photoURL} alt={"avatar"} style={{ width: "50px" }} />
+      <small>{displayName}</small>
+      <p>{text}</p>
+    </div>
+  );
+}
 
 function SignIn() {
   const signInWithGoogle = () => {
